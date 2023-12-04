0eb6f759dea75018093e158a56eb71e17872d4c7
Simon <sillen.00@hotmail.com>
2023-12-04 10:50:44 +0100

    Updated npm and setup for test fix.

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,9 @@
+// import { render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+// import userEvent from "@testing-library/user-event";
+
+describe("something truthy and falsy", () => {
+    it("true to be true", () => {
+        expect(true).toBe(true);
+    });
+});
