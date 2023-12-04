f5409258955c0ecd65d754eddcdc2f164a710d41
Simon <sillen.00@hotmail.com>
2023-12-04 11:21:38 +0100

    mantine install and theme setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
+
+import "@mantine/carousel/styles.css";
+import "@mantine/core/styles.css";
+import "@mantine/form/styles.css";
+import "@mantine/hooks/styles.css";
 
 function App() {
-
-  return (
-    <div className="App">
-      <h1>Netflix 2.0</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>Netflix 2.0</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./mantineTheme";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <MantineProvider theme={theme}>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>
+);
