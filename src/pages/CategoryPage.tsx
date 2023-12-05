44b284f9ac9736e9a9ec5dd97e77561a2164f449
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-05 14:11:40 +0100

    add functionality to filter movies based on genre

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,9 +1,17 @@
-import CategoryButton from "../components/CategoryButton";
+import { useState } from "react";
+import CategoryButton from "../components/CategoryButton/CategoryButton";
+import MovieCard from "../components/MovieCard";
 
 function CategoryPage() {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
   return (
     <div>
-      <CategoryButton />
+      <CategoryButton setSelectedGenre={setSelectedGenre} />
+      <MovieCard selectedGenre={selectedGenre} />
+      {/* lägg till detta i moviecard 
+      function MovieCard({ selectedGenre }: { selectedGenre: string | null }) {
+      */}
     </div>
   );
 }
