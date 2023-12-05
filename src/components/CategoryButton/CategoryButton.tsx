859104cb44c4625a5e88ab8bda7a1df01a6416b0
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-05 14:54:30 +0100

    add a select that is shown on smaller screens, to make page more responsive

--- src/components/CategoryButton/CategoryButton.tsx
+++ src/components/CategoryButton/CategoryButton.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@mantine/core";
+import { Button, Select } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { useContext } from "react";
 import { MovieContext } from "../../contexts/MovieContext";
-import { StyledButton, StyledButtonContainer } from "./CategoryButton.style";
+import { StyledButton, StyledButtonContainer, StyledSelect } from "./CategoryButton.style";
 
 interface CategoryButtonProps {
   setSelectedGenre: (genre: string | null) => void;
@@ -9,9 +10,9 @@ interface CategoryButtonProps {
 
 function CategoryButton({ setSelectedGenre }: CategoryButtonProps) {
   const { movies } = useContext(MovieContext);
+  const isSmallScreen = useMediaQuery("(max-width: 1210px)");
 
   const allGenres = movies.flatMap(movie => movie.genre.split(", "));
-
   const uniqueGenres = [...new Set(allGenres)];
 
   const handleGenreClick = (genre: string) => {
@@ -20,13 +21,24 @@ function CategoryButton({ setSelectedGenre }: CategoryButtonProps) {
 
   return (
     <StyledButtonContainer>
-      {uniqueGenres.map(genre => (
-        <div key={genre}>
-          <StyledButton>
-            <Button onClick={() => handleGenreClick(genre)}>{genre}</Button>
-          </StyledButton>
-        </div>
-      ))}
+      {isSmallScreen ? (
+        <StyledSelect>
+          <Select
+            label='Select Genre'
+            placeholder='Select Genre'
+            data={uniqueGenres}
+            onChange={value => handleGenreClick(value as string)}
+          />
+        </StyledSelect>
+      ) : (
+        uniqueGenres.map(genre => (
+          <div key={genre}>
+            <StyledButton>
+              <Button onClick={() => handleGenreClick(genre)}>{genre}</Button>
+            </StyledButton>
+          </div>
+        ))
+      )}
     </StyledButtonContainer>
   );
 }
