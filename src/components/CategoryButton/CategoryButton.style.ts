f908e562c28f4c61060af63d30121a7c9ba3b698
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-05 14:10:53 +0100

    add styling to category button

---
+++ src/components/CategoryButton/CategoryButton.style.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const StyledButton = styled.div`
+  .mantine-Button-root {
+    background: none;
+    color: #fff;
+    border: none;
+    font-weight: 400;
+  }
+
+  .mantine-Button-root:focus {
+    font-weight: 800;
+  }
+`;
+
+export const StyledButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
