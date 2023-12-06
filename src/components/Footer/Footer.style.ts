2503b736ca991cd0726837999639579f37e33654
Simon <sillen.00@hotmail.com>
2023-12-06 16:12:21 +0100

    footer styles

--- src/components/Footer/Footer.style.ts
+++ src/components/Footer/Footer.style.ts
@@ -1,9 +1,17 @@
 import styled from "@emotion/styled";
 
 export const StyledFooter = styled.footer`
-  background-color: green;
+  background-color: black;
+  color: white;
 
-  .link-box {
-    
+  min-height: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10%;
+
+  .link-box a{
+    color: white;
+    /* text-decoration: none; */
   }
 `;
