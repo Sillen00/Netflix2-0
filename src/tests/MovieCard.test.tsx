6d4bc6a795509ccef592f6b155601ed016297485
Simon <sillen.00@hotmail.com>
2023-12-06 09:43:59 +0100

    mantine provider fix om moviecard test.

--- src/tests/MovieCard.test.tsx
+++ src/tests/MovieCard.test.tsx
@@ -1,28 +1,28 @@
-import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import MovieCard from "../components/MovieCard/MovieCard";
+import { render, screen } from "../utils/test-utils";
 
 interface MovieCardTestProps {
-    thumbnail: string; // Make it non-optional
-    year: string;
-    age: string;
+  thumbnail: string; // Make it non-optional
+  year: string;
+  age: string;
 }
 
 it("true to be true", () => {
-    expect(true).toBe(true);
+  expect(true).toBe(true);
 });
 
 describe("MovieCard", () => {
-    it("should render the movie title", () => {
-        const movieProps: MovieCardTestProps = {
-            thumbnail: 'https://example.com/image.jpg', 
-            year: '2022',
-            age: 'PG-13',
-        };
-        
-        render(<MovieCard {...movieProps} />);
-        
-        expect(screen.getByText('2022')).toBeInTheDocument();
-        expect(screen.getByText('PG-13')).toBeInTheDocument();
-    });
+  it("should render the movie title", () => {
+    const movieProps: MovieCardTestProps = {
+      thumbnail: "https://example.com/image.jpg",
+      year: "2022",
+      age: "PG-13",
+    };
+
+    render(<MovieCard {...movieProps} />);
+
+    expect(screen.getByText("2022")).toBeInTheDocument();
+    expect(screen.getByText("PG-13+")).toBeInTheDocument();
+  });
 });
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,8 +1,20 @@
-import { afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/react";
+import { afterEach, vi } from "vitest";
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
+});
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
 });
