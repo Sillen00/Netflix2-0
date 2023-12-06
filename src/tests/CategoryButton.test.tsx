36fb48b26af02090f2337e18c0b0b732ac758db2
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-06 10:35:16 +0100

    change test to have a describe and it instead of test

--- src/tests/CategoryButton.test.tsx
+++ src/tests/CategoryButton.test.tsx
@@ -1,7 +1,18 @@
 import { render } from "@testing-library/react";
-import { test } from "vitest";
+import { describe, it } from "vitest";
 import CategoryButton from "../components/CategoryButton/CategoryButton";
+import { StyledSelect } from "../components/CategoryButton/CategoryButton.style";
 
-test("should see if the category buttons exists", () => {
-  render(<CategoryButton setSelectedGenre={() => {}} />);
+describe("Category Button", () => {
+  it("should see if the category buttons exist on a screen with width larger than 1210px", () => {
+    global.innerWidth = 1220;
+
+    render(<CategoryButton setSelectedGenre={() => {}} />);
+  });
+
+  it("should see if the select exist on a screen with width smaller than 1210px", () => {
+    global.innerWidth = 1200;
+
+    render(<StyledSelect />);
+  });
 });
