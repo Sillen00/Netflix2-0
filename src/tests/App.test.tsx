33d7a72e065b98182c4d4482e8ec9a6084b85fb7
Leon Björklund <leon.bjorklund@gmail.com>
2023-12-06 17:18:07 +0100

    search-test moved to App.test & created header.test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,21 @@
-// import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
-// import userEvent from "@testing-library/user-event";
+import Header from "../components/Header/Header";
+import { render, screen } from "../utils/test-utils";
 
-describe("something truthy and falsy", () => {
-  it("true to be true", () => {
-    expect(true).toBe(true);
+describe("App Functionality", () => {
+  it("should render the search results", async () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    const searchInput = screen.getByPlaceholderText("Search...");
+    await userEvent.type(searchInput, "The Matrix");
+
+    const image = await screen.findByAltText("The Matrix");
+    expect(image).toBeInTheDocument();
   });
 });
