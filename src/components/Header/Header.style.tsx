78aa972b0ed4e6bd3e41ef30f88108734880c1d4
Leon Björklund <leon.bjorklund@gmail.com>
2023-12-06 13:38:26 +0100

    responsive

--- src/components/Header/Header.style.tsx
+++ src/components/Header/Header.style.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { Input, InputProps, Title } from "@mantine/core";
 import { NavLink } from "react-router-dom";
+import breakpoints from "../../utils/breakpoints";
 
 // Header styles:
 
@@ -10,12 +11,39 @@ export const StyledHeader = styled.header`
   align-items: center;
   padding: 0 1.5rem;
   height: 6rem;
+  gap: 0.75rem;
   color: white;
   > div {
-    gap: 1rem;
+    gap: 1.5rem;
+    justify-content: flex-end;
+    flex-wrap: nowrap;
   }
   > a {
     text-decoration: none;
+  }
+  @media (max-width: ${breakpoints.md}) {
+    > div {
+      gap: 1rem;
+    }
+  }
+  @media (max-width: ${breakpoints.sm}) {
+    height: 5rem;
+    > div {
+      gap: 0.75rem;
+    }
+    .hide-on-search {
+      display: none;
+    }
+  }
+  @media (max-width: ${breakpoints.xs}) {
+    height: 4rem;
+    padding: 0 0.75rem;
+    > div {
+      gap: 0.5rem;
+    }
+    .hide-on-search {
+      display: none;
+    }
   }
 `;
 
@@ -23,7 +51,19 @@ export const StyledTitle = styled(Title)`
   font-family: "Bebas Neue", sans-serif;
   font-size: 3.5rem;
   font-weight: 500;
+  white-space: nowrap;
   color: white;
+  line-height: normal;
+  @media (max-width: ${breakpoints.md}) {
+    font-size: 3.25rem;
+  }
+  @media (max-width: ${breakpoints.sm}) {
+    font-size: 3rem;
+  }
+  @media (max-width: ${breakpoints.xs}) {
+    font-size: 2.25rem;
+    padding-top: 0.25rem;
+  }
 `;
 
 export const StyledNavLink = styled(NavLink)`
@@ -32,6 +72,12 @@ export const StyledNavLink = styled(NavLink)`
   text-decoration: none;
   &:hover {
     opacity: 0.7;
+  }
+  @media (max-width: ${breakpoints.sm}) {
+    font-size: 1.15rem;
+  }
+  @media (max-width: ${breakpoints.xs}) {
+    font-size: 1rem;
   }
 `;
 
@@ -49,18 +95,28 @@ interface StyledInputProps extends InputProps {
 export const StyledInput = styled(Input, {
   shouldForwardProp: prop => !["isSearchOpen"].includes(prop),
 })<StyledInputProps>`
-  input {
+  .mantine-Input-input {
     background-color: transparent;
     border: ${props => (props.isSearchOpen ? "1px solid rgb(255,255,255,0.6)" : "none")};
     color: white;
-    width: ${props => (props.isSearchOpen ? "300px" : "0")};
+    width: ${props => (props.isSearchOpen ? "280px" : "0")};
     transition: width 0.2s;
     min-height: 20px;
     height: 38px;
     padding-top: 2px;
     padding-left: ${props => (props.isSearchOpen ? "40px" : "20px")};
+    @media (max-width: ${breakpoints.sm}) {
+      width: ${props => (props.isSearchOpen ? "260px" : "0")};
+      transition: width 0s; /* Instant transition */
+    }
+    @media (max-width: ${breakpoints.xs}) {
+      transition: width 0s; /* Instant transition */
+      height: 34px;
+    }
+    @media (max-width: 424px) {
+      width: ${props => (props.isSearchOpen ? "100%" : "0")};
+    }
   }
-
   .search-action-icon {
     &:hover {
       opacity: 0.7;
