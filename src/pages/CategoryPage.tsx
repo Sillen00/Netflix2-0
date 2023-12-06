0ea50402eef4fa74b9dbbc457e23cb12f5febc22
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-06 15:02:08 +0100

    add filter and grid for moviecards

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { Grid } from "@mantine/core";
+import { useContext, useState } from "react";
 import CategoryButton from "../components/CategoryButton/CategoryButton";
-import MovieCard from "../components/MovieCard";
+import MovieCard from "../components/MovieCard/MovieCard";
+import { MovieContext } from "../contexts/MovieContext";
 
 function CategoryPage() {
-  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
+  const { movies } = useContext(MovieContext);
+
+  // Filter movies based on selected genre
+  const filteredMovies = selectedGenre
+    ? movies.filter(movie => movie.genre.includes(selectedGenre))
+    : movies;
 
   return (
-    <div>
+    <div style={{ padding: "1rem" }}>
       <CategoryButton setSelectedGenre={setSelectedGenre} />
-      <MovieCard selectedGenre={selectedGenre} />
-      {/* lägg till detta i moviecard 
-      function MovieCard({ selectedGenre }: { selectedGenre: string | null }) {
-      */}
+      <Grid gutter={20}>
+        {filteredMovies.map(movie => (
+          <Grid.Col span={{ base: 6, md: 3, lg: 2.4 }} key={movie.title}>
+            <MovieCard {...movie} />
+          </Grid.Col>
+        ))}
+      </Grid>
     </div>
   );
 }
