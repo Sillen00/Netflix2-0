9bfbc1e49904e0b3d92e5a0ae0039965203ac492
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-06 15:35:12 +0100

    change test after styling changes

--- src/tests/CategoryButton.test.tsx
+++ src/tests/CategoryButton.test.tsx
@@ -1,18 +1,9 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
 import CategoryButton from "../components/CategoryButton/CategoryButton";
-import { StyledSelect } from "../components/CategoryButton/CategoryButton.style";
 
 describe("Category Button", () => {
-  it("should see if the category buttons exist on a screen with width larger than 1210px", () => {
-    global.innerWidth = 1220;
-
+  it("should see if the category buttons renders", () => {
     render(<CategoryButton setSelectedGenre={() => {}} />);
-  });
-
-  it("should see if the select exist on a screen with width smaller than 1210px", () => {
-    global.innerWidth = 1200;
-
-    render(<StyledSelect />);
   });
 });
