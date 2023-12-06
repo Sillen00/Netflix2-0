bf852cdf0146adb9d2e18804cfd42d7f627adbab
Simon <sillen.00@hotmail.com>
2023-12-06 09:45:16 +0100

    i used startpage to check how the moviecard looked. Now i removed moviecard again

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,16 +1,7 @@
-import MovieCard from "../components/MovieCard/MovieCard";
-
 function StartPage() {
   return (
     <div>
       <h1>StartPage</h1>
-      <MovieCard
-        thumbnail={
-          "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX380_CR0,4,380,562_.jpg"
-        }
-        year={"1994"}
-        age={"15"}
-      />
     </div>
   );
 }
