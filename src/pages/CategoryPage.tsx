9ae7b406b35ac259f7c4be34b629b33978584fac
Moa Hedendahl <moahedendahl@gmail.com>
2023-12-07 09:34:48 +0100

    moved functionality to a component instead of handling it on the page

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,28 +1,9 @@
-import { Grid } from "@mantine/core";
-import { useContext, useState } from "react";
-import CategoryButton from "../components/CategoryButton/CategoryButton";
-import MovieCard from "../components/MovieCard/MovieCard";
-import { MovieContext } from "../contexts/MovieContext";
+import CategoryMovieList from "../components/CategoryMovieList/CategoryMovieList";
 
 function CategoryPage() {
-  const [selectedGenre, setSelectedGenre] = useState<string | null>(null);
-  const { movies } = useContext(MovieContext);
-
-  // Filter movies based on selected genre
-  const filteredMovies = selectedGenre
-    ? movies.filter(movie => movie.genre.includes(selectedGenre))
-    : movies;
-
   return (
-    <div style={{ padding: "1rem" }}>
-      <CategoryButton setSelectedGenre={setSelectedGenre} />
-      <Grid gutter={20}>
-        {filteredMovies.map(movie => (
-          <Grid.Col span={{ base: 6, md: 3, lg: 2.4 }} key={movie.title}>
-            <MovieCard {...movie} />
-          </Grid.Col>
-        ))}
-      </Grid>
+    <div>
+      <CategoryMovieList />
     </div>
   );
 }
