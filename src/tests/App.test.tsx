9dc561552afbaf0d948d557dd11207ca6c906d29
edvindjulic <djulicedvin@gmail.com>
2023-12-11 15:15:30 +0100

    Navigation-tests. CategoryButton test and CategoryPage test.
    Co-authored-by: Moa Hedendahl <moa.hedendahl@medieinstitutet.se>

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,7 @@
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { describe, expect, it } from "vitest";
+import App from "../App";
 import Header from "../components/Header/Header";
 import BookmarkedPage from "../pages/BookMarkedPage";
 import CategoryPage from "../pages/CategoryPage";
@@ -41,86 +42,125 @@ describe("App Functionality", () => {
   });
 });
 
-it("should be able to navigate from startpage to categorypage", async () => {
-  render(
-    <MemoryRouter>
-      <Header />
-      <Routes>
-        <Route index element={<StartPage />} />
-        <Route path='categories' element={<CategoryPage />} />
-      </Routes>
-    </MemoryRouter>
-  );
-  const user = userEvent.setup();
-  await user.click(screen.getByText("Categories"));
-  expect(screen.getByRole("button", { name: "Action" })).toBeInTheDocument();
-});
-it("should be able to navigate from startpage to bookmarkedpage", async () => {
-  render(
-    <MemoryRouter>
-      <Header />
-      <Routes>
-        <Route index element={<StartPage />} />
-        <Route path='bookmarks' element={<BookmarkedPage />} />
-      </Routes>
-    </MemoryRouter>
-  );
-  const user = userEvent.setup();
-  await user.click(screen.getByText("Bookmarks"));
-  expect(screen.getByText("You dont have any movies bookmarked.")).toBeInTheDocument();
-});
-it("should be able to add a movie to bookmarks and navigate to the bookmarkedpage", async () => {
-  render(
-    <MemoryRouter>
-      <Header />
-      <Routes>
-        <Route index element={<StartPage />} />
-        <Route path='bookmarks' element={<BookmarkedPage />} />
-      </Routes>
-    </MemoryRouter>
-  );
-  const user = userEvent.setup();
-  const matrixMovieCard = await screen.findByTestId("id-The Matrix");
-  const matrixBookmarkButton = within(matrixMovieCard).getByTestId("bookmark-icon");
-  await user.click(matrixBookmarkButton);
-  await user.click(screen.getByText("Bookmarks"));
-  expect(screen.getByText("The Matrix")).toBeInTheDocument();
+describe("Navigation between startpage and categorypage", () => {
+  it("should be able to navigate from startpage to categorypage", async () => {
+    render(
+      <MemoryRouter>
+        <Header />
+        <Routes>
+          <Route index element={<StartPage />} />
+          <Route path='categories' element={<CategoryPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    await user.click(screen.getByText("Categories"));
+    expect(screen.getByRole("button", { name: "Action" })).toBeInTheDocument();
+  });
+  it("should be able to navigate from categorypage to startpage", async () => {
+    render(
+      <MemoryRouter initialEntries={["/categories"]}>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<StartPage />} />
+            <Route path='categories' element={<CategoryPage />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("heading", { name: "NETFLIX 2.0" }));
+    expect(screen.getByText("Trending")).toBeInTheDocument();
+  });
 });
 
-it("should be able to navigate from categorypage to startpage", async () => {
-  render(
-    <MemoryRouter>
-      <Header />
-      <Routes>
-        <Route index element={<StartPage />} />
-        <Route path='categories' element={<CategoryPage />} />
-      </Routes>
-    </MemoryRouter>
-  );
-  const user = userEvent.setup();
-  await user.click(screen.getByText("Categories"));
-  expect(screen.getByRole("button", { name: "Action" })).toBeInTheDocument();
-  await user.click(screen.getByRole("heading", { name: "NETFLIX 2.0" }));
-  expect(screen.getByText("Trending")).toBeInTheDocument();
+describe("Navigation between startpage and bookmarkedpage", () => {
+  it("should be able to navigate from startpage to bookmarkedpage", async () => {
+    render(
+      <MemoryRouter>
+        <Header />
+        <Routes>
+          <Route index element={<StartPage />} />
+          <Route path='bookmarks' element={<BookmarkedPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    await user.click(screen.getByText("Bookmarks"));
+    expect(screen.getByText("You dont have any movies bookmarked.")).toBeInTheDocument();
+  });
+  it("should be able to navigate from bookmarkedpage to startpage", async () => {
+    render(
+      <MemoryRouter initialEntries={["/bookmarks"]}>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<StartPage />} />
+            <Route path='bookmarks' element={<BookmarkedPage />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("heading", { name: "NETFLIX 2.0" }));
+    expect(screen.getByText("Trending")).toBeInTheDocument();
+  });
 });
 
-it("should be able to navigate from startpage to movieviewpage", async () => {
+describe("should be able to navigate between startpage and movieviewpage", () => {
+  it("should be able to navigate from startpage to movieviewpage", async () => {
+    render(
+      <MemoryRouter>
+        <Header />
+        <Routes>
+          <Route index element={<StartPage />} />
+          <Route path='movie/:slug' element={<MovieViewPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    const godFatherMovieCard = await screen.findByTestId("id-The Godfather: Part II");
+    const godFatherMovieCardImg = within(godFatherMovieCard).getByRole("img");
+    await user.click(godFatherMovieCardImg);
+    expect(
+      screen.getByText(
+        "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate."
+      )
+    ).toBeInTheDocument();
+  });
+  it("should be able to navigate from movieviewpage to startpage", async () => {
+    render(
+      <MemoryRouter initialEntries={["/movie/some-movie"]}>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<StartPage />} />
+            <Route path='movie/:slug' element={<MovieViewPage />} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("heading", { name: "NETFLIX 2.0" }));
+    expect(screen.getByText("Trending")).toBeInTheDocument();
+  });
+});
+it("should be able to navigate from categories to movieviewpage", async () => {
   render(
     <MemoryRouter>
       <Header />
       <Routes>
-        <Route index element={<StartPage />} />
+        <Route index element={<CategoryPage />} />
         <Route path='movie/:slug' element={<MovieViewPage />} />
       </Routes>
     </MemoryRouter>
   );
   const user = userEvent.setup();
-  const goodFellasMovieCard = await screen.findByTestId("id-Goodfellas");
-  const goodFellasImg = within(goodFellasMovieCard).getByAltText("Goodfellas");
-  await user.click(goodFellasImg);
+  const godFatherMovieCard = await screen.findByTestId("id-The Godfather: Part II");
+  const godFatherMovieCardImg = within(godFatherMovieCard).getByRole("img");
+  await user.click(godFatherMovieCardImg);
   expect(
     screen.getByText(
-      "The story of Henry Hill and his life in the mafia, covering his relationship with his wife Karen and his mob partners Jimmy Conway and Tommy DeVito."
+      "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate."
     )
   ).toBeInTheDocument();
 });
--- src/tests/CategoryButton.test.tsx
+++ src/tests/CategoryButton.test.tsx
@@ -1,9 +1,10 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { render, screen} from "../utils/test-utils";
+import { describe, expect, it } from "vitest";
 import CategoryButton from "../components/CategoryButton/CategoryButton";
 
 describe("Category Button", () => {
   it("should see if the category buttons renders", () => {
     render(<CategoryButton setSelectedGenre={() => {}} />);
   });
+  expect(screen.getByRole("button", { name: "Action" })).toBeInTheDocument();
 });
