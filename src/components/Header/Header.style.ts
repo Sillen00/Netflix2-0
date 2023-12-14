fb48e226e2871134736750fc71da31d6ee2534cd
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-12-14 09:07:07 +0100

    Refactor header styling

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -7,26 +7,9 @@ export const StyledHeader = styled.header`
   align-items: center;
   padding: 0 1.5rem;
   height: 6rem;
-  font-size: 1.3rem;
   gap: 0.5rem;
-
-  > div {
-    gap: 1.5rem;
-    justify-content: flex-end;
-    flex-wrap: nowrap;
-  }
-
-  @media (max-width: ${breakpoints.md}) {
-    > div {
-      gap: 1rem;
-    }
-  }
-
   @media (max-width: ${breakpoints.sm}) {
     height: 5rem;
-    > div {
-      gap: 0.75rem;
-    }
     .hide-on-search {
       display: none;
     }
@@ -34,7 +17,21 @@ export const StyledHeader = styled.header`
   @media (max-width: ${breakpoints.xs}) {
     height: 4rem;
     padding: 0 0.75rem;
-    > div {
+  }
+
+  > div {
+    gap: 1.5rem;
+    justify-content: flex-end;
+    flex-wrap: nowrap;
+    font-size: 1.3rem;
+
+    @media (max-width: ${breakpoints.md}) {
+      gap: 1rem;
+    }
+    @media (max-width: ${breakpoints.sm}) {
+      gap: 0.75rem;
+    }
+    @media (max-width: ${breakpoints.xs}) {
       gap: 0.5rem;
     }
   }
@@ -43,7 +40,6 @@ export const StyledHeader = styled.header`
     &:hover {
       opacity: 0.7;
     }
-
     @media (max-width: ${breakpoints.sm}) {
       font-size: 1.15rem;
     }
@@ -57,16 +53,6 @@ export const StyledHeader = styled.header`
     font-size: 3.5rem;
     font-weight: 500;
     white-space: nowrap;
-
-    @media (max-width: ${breakpoints.md}) {
-      font-size: 3.25rem;
-    }
-    @media (max-width: ${breakpoints.sm}) {
-      font-size: 3rem;
-    }
-    @media (max-width: ${breakpoints.xs}) {
-      font-size: 2.125rem;
-      padding-top: 0.25rem;
-    }
+    font-size: clamp(2.125rem, (6vw), 3.5rem);
   }
 `;
